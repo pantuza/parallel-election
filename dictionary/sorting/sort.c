5490e20e04b36870ef7ab9cb4ab799615c2177e9
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2014-05-29 23:53:12 -0300

    Refactoring sort algorithm

--- dictionary/sorting/sort.c
+++ dictionary/sorting/sort.c
@@ -1,35 +1,46 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "palavras.h"
+
+
+#include "words.h"
+
 
 int main(int argc, char *argv[])
 
 {
-	FILE *in,*out;
-	int i,j;
+	FILE *in, *out;
+	int i, j;
 	palavra e;
-	palavras *dicionario;
+	words *dict;
 	char string[256];
 	
-	in=fopen(argv[1],"r");
-	if(!in)return 404;
-	sprintf(string,"%s_ordenado",argv[1]);
-	out=fopen(string,"w");
-	dicionario=carregar_palavras(in);
-//insertion
-	for(i=1; i< dicionario->n ; i++)
+	in = fopen(argv[1], "r");
+	if(!in)
+        return 404;
+
+	sprintf(string, "%s_ordenado", argv[1]);
+	out = fopen(string, "w");
+	dict = load_words(in);
+
+    /* insertion */
+	for(i = 1; i < dict->n; i++)
 	{
-		e = dicionario->p[i];
-		j=i-1;
-		while((j>=0) && (comparar_palavra(e.string , dicionario->p[j].string)>0))
+		e = dict->p[i];
+		j = i - 1;
+		while((j >= 0) && (compare(e.string, dict->p[j].string) > 0))
 		{
-			dicionario->p[j+1] = dicionario->p[j];
+			dict->p[j+1] = dict->p[j];
 			j--;
 		}
-		dicionario->p[j+1] = e;
+		dict->p[j+1] = e;
 	}
-	for (i=0;i<dicionario->n;i++)
-		fprintf(out,"%d\t%s\n",dicionario->p[i].valor,dicionario->p[i].string);
-	fclose(in);
+
+	for (i = 0; i < dict->n; i++)
+		fprintf(out, "%d\t%s\n", dict->p[i].valor, dict->p[i].string);
+	
+    fclose(in);
 	fclose(out);
+
+    return EXIT_SUCCESS;
 }
