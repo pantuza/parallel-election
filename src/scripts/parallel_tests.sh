b18e955082de19bdadc53505c503d63851bd7a97
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2014-06-05 11:28:39 -0300

    Adding test script for parallel approach

---
+++ src/scripts/parallel_tests.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+
+#
+# Script to run tests on parallel approach
+#
+
+
+#
+# Variables
+#
+IN_DIR="input";
+OUT_DIR="output";
+BIN="../parallel/analyser";
+DICT="../../in/dictionary";
+# candidates
+C1="../../in/dilma.txt";
+C2="../../in/aecio.txt";
+C3="../../in/eduardo.txt";
+OUT_FILE="parallel_time_execution.csv";
+
+
+#
+# Creates the output directory 
+#
+mkdir -p $OUT_DIR;
+
+
+#
+# Verifies if there is a file
+#
+[ ! -f "$BIN" ] && echo "There is no file $BIN" && exit 1;
+
+
+#
+# Verifies if the file is binary
+#
+[ ! -x "$BIN" ] && echo "Not a binary file $BIN" && exit 2;
+
+
+#
+# Verifies if the input dir is empty
+#
+[ ! "$(ls -A $IN_DIR)" ] && echo "No input files on $IN_DIR/" && exit 3;
+
+
+#
+# Creates the output file with CSV headers
+#
+echo "Tamanho da entrada,Tempo em segundos" > $OUT_DIR/$OUT_FILE;
+
+
+#
+# Runs the parallel program for each input file 
+#
+ls -tr $IN_DIR | while read infile;
+do
+    # Number of lines of this input file
+    NLINES=$(wc -l $IN_DIR/$infile | cut -d' ' -f1);
+    
+    # Execution time of this input file
+    TIME=$(./$BIN $IN_DIR/$infile $DICT $C1 $C2 $C3 |grep time |cut -d' ' -f3);
+    
+    # writes to outfile
+    echo "$NLINES,$TIME" >> $OUT_DIR/$OUT_FILE;
+
+    echo -e "$infile -> ($NLINES, $TIME)";
+done;
+
+echo -e "Result file placed at $OUT_DIR/$OUT_FILE";
